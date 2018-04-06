55f28979e8535434d5c1318c79b334d4c2cf13a0
Rodolphe Nemr <Rodmack@rodolphes-macbook-pro.local>
2018-04-06 15:46:41 +0300

    Initial Commit

---
+++ 22/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  22
+//
+//  Created by Rodolphe Nemr on 4/6/18.
+//  Copyright © 2018 Rodolphe Nemr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
